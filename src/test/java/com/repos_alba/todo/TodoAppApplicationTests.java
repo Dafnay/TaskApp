02318c8f040fdf4692221184a81a50f5fefb9e2a
Dafnay <albavelascoglez@gmail.com>
2026-01-12 12:58:20 +0100

    Initial commit + task template approach

---
+++ src/test/java/com/repos_alba/todo/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repos_alba.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
